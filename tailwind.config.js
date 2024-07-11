75b2bf2af4b75eed8a65303556ce47018f5289f8
AryanshDev <aryanshappmaker@gmail.com>
2024-07-11 10:40:27 +0530

    Added Gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ['./src/*.html', './src/*.ejs'],
   theme: {
     extend: {
-      
+        animation: {
+          'gradient-6': 'gradient 6s linear infinite',
+          'gradient-4': 'gradient 4s linear infinite',
+        },
+        keyframes: {
+          'gradient': {
+            to: { 'background-position': '200% center' },
+          }
+        }                    
     },
   },
 
